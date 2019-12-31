b06cc064ad0a18892a53ba0838274350c9c7d0df
gregorycernera <cernerag@gmail.com>
2019-12-30 19:18:13 -0500

    Added icons to the sidebar

--- cernera_me/src/components/sidebar/Sidebar.tsx
+++ cernera_me/src/components/sidebar/Sidebar.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import style from './Sidebar.module.scss';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faBookOpen, faHome, faAddressCard, faTools, faUserTie, faPhone } from '@fortawesome/free-solid-svg-icons'
+library.add(fab, faBookOpen, faHome, faAddressCard, faTools, faUserTie, faPhone)
+
 const Sidebar: React.FC = () => {
     return (
         <div id="Sidebar">
             <div className={style["sidebar"]}>
-                <a className={style["sidebar__menu-item"]} href="/">Home</a>
-                <a className={style["sidebar__menu-item"]} href="/about">About</a>
-                <a className={style["sidebar__menu-item"]} href="#">Services</a>
-                <a className={style["sidebar__menu-item"]} href="#">Clients</a>
-                <a className={style["sidebar__menu-item"]} href="#">Contact</a>
+
+                <a className={style["sidebar__menu-item"]} href="/">
+                    <FontAwesomeIcon icon={faHome} size='sm' style={{marginRight: '8%'}}/>Home</a>
+                <hr className={style["sidebar__horizontal-bar"]}></hr>
+
+                <a className={style["sidebar__menu-item"]} href="/about">
+                    <FontAwesomeIcon icon={faAddressCard} size='sm' style={{marginRight: '8%'}}/>About</a>
+                <hr className={style["sidebar__horizontal-bar"]}></hr>
+
+                <a className={style["sidebar__menu-item"]} href="#">
+                    <FontAwesomeIcon icon={faTools} size='sm' style={{marginRight: '8.5%'}}/>Services</a>
+                <hr className={style["sidebar__horizontal-bar"]}></hr>
+
+                <a className={style["sidebar__menu-item"]} href="#">
+                    <FontAwesomeIcon icon={faUserTie} size='sm' style={{marginRight: '9%'}}/>Clients</a>
+                <hr className={style["sidebar__horizontal-bar"]}></hr>
+
+                <a className={style["sidebar__menu-item"]} href="#">
+                    <FontAwesomeIcon icon={faPhone} size='sm' style={{marginRight: '8%'}}/>Contact</a>
+                <hr className={style["sidebar__horizontal-bar"]}></hr>
+
             </div>
         </div>
     );
