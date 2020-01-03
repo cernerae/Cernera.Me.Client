a67a4d3b38e23e6ff4798951ed3aedb2a5088afb
Edward Cernera <cernerae@gmail.com>
2020-01-02 20:51:30 -0500

    Formatted repository cards

--- cernera_me/src/components/github/RepositoryCard.tsx
+++ cernera_me/src/components/github/RepositoryCard.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import { Container, Card } from "react-bootstrap";
+import { Container, Card, Col, Row } from "react-bootstrap";
+import github_logo from "../../assets/images/github_logo.png";
 import { GitHubRepository } from "../../types";
 import styles from './Github.module.scss';
 
 export const RepositoryCard = ({ repo }: { repo: GitHubRepository }) => {
 
     return (
-        <Card className={styles["repository-card"]}>
-            <Card.Title><a href={repo.html_url} target="_blank">{repo.name}</a></Card.Title>
-            <Card.Body>{repo.description}</Card.Body>
+        <Card className={[styles["repository-card"], "shadow-sm"].join(' ')}>
+            <a href={repo.html_url} target="_blank">
+                <Card.Title>{repo.name}</Card.Title>
+                <Card.Body>
+                    <Container>
+                        <Row>
+                            <Col md={9}>{repo.description}</Col>
+                            <Col md={3}><img className={styles["repository-card__github-logo"]} src={github_logo} /></Col>
+                        </Row>
+                    </Container>
+                </Card.Body>
+            </a>
         </Card>
     );
 }
--- cernera_me/src/components/github/RepositoryCardList.tsx
+++ cernera_me/src/components/github/RepositoryCardList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Card } from "react-bootstrap";
+import { Container, Card, Col, Row } from "react-bootstrap";
 import { GitHubRepository } from "../../types/index";
 import { RepositoryCard } from "./RepositoryCard";
 import styles from './Github.module.scss';
@@ -7,12 +7,14 @@ import styles from './Github.module.scss';
 export const RepositoryCardList = ({ repositories }: { repositories: GitHubRepository[] }) => {
 
     const cards = repositories
-        ? (repositories.map((repo: GitHubRepository) => <RepositoryCard repo={repo} />))
+        ? (repositories.map((repo: GitHubRepository, index: number) => <Col md={6}><RepositoryCard repo={repo} /></Col>))
         : null;
 
     return (
         <div id="RepositoryCardList" className={styles["repository-card-list"]}>
-            <Container>{cards}</Container>
+            <Container>
+                <Row>{cards}</Row>
+            </Container>
         </div>
     );
 }
