1b89ecc146d63e655e30128f9b558c7586f6a3d9
gregorycernera <cernerag@gmail.com>
2020-01-10 22:53:33 -0500

    Updated the Greg landing page to follow Ed's style

--- cernera_me/src/components/pages/users/gregory/GregoryLandingPage.tsx
+++ cernera_me/src/components/pages/users/gregory/GregoryLandingPage.tsx
@@ -1,40 +1,45 @@
-import React from 'react';
-import { Container, Row, Col, Accordion, Card, Button } from "react-bootstrap";
+import React, { useEffect, useState } from 'react';
+import { connect, useDispatch, useStore, useSelector } from "react-redux";
+import { Container, Row, Col, Jumbotron, Button, ButtonGroup, Image } from "react-bootstrap";
 import Sidebar from "components/sidebar/Sidebar";
-import style from './GregoryLandingPage.module.scss';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-library.add(fab, faEnvelope)
+import styles from 'components/pages/LandingPage.module.scss';
+import MainTextBlock from "components/text/MainTextBlock";
+import { getGitHubRepositoriesAction } from "store/actions/actionCreators";
+import { RepositoryCardList } from "components/github/RepositoryCardList";
+import generalStyles from "components/General.module.scss";
 
 const GregoryLandingPage: React.FC = () => {
+
+    const store: any = useSelector(state => state);
+    const dispatch = useDispatch();
+    const [showText, setShowText] = useState("hide-me");
+    const [showSidebar, setShowSidebar] = useState("hide-left");
+
+    const textBody = `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+        aliquip ex ea commodo consequat.
+        `
+
+    useEffect(() => {
+        console.log("Getting GitHub Repositories on load...")
+        dispatch(getGitHubRepositoriesAction("cernerae"));
+        setTimeout(() => setShowText("show-me"), 500);
+        setTimeout(() => setShowSidebar("slide-right"), 250);
+    }, []);
+
     return (
-        <div id="GregoryLandingPage" className={style["gregory-landing-page"]}>
-            <div className={style["gregory-landing-page__content"]}>
-                <Container fluid>
-                    <Row className="show-grid">
-
-                        <Col style={{ maxWidth: '260px' }}><Sidebar sm={true} /></Col>
-
-                        <Col>
-
-
-                            <h1 className={style["gregory-landing-page__content__title"]}>
-                                Hi, my name is
-                                <br></br>
-                                <div style={{ fontSize: '80px', color: '#bb86fc' }}>Gregory Cernera</div>
-                            </h1>
-
-                            <h1 className={style["gregory-landing-page__content__body-text"]}>I am a Software Engineer</h1>
-
-                            <Button className={style["gregory-landing-page__content__contact-btn"]} href='/contact'> <FontAwesomeIcon icon={faEnvelope} size='sm' /> Contact Me </Button>
-
-
-
+        <div id="EdwardLandingPage" className={styles["landing-page"]}>
+            <Sidebar className={generalStyles[`${showSidebar}`]} sm={true} />
+            <div className={styles["landing-page__content-greg"]}>
+                <Container className={styles["landing-page__content__container"]}>
+                    <Row className="h-100">
+                        <Col md={6} className={styles["landing-page__content__container__vertical-center"]}>
+                            <MainTextBlock className={generalStyles[`${showText}`]} title={"Gregory Cernera"} text={textBody} />
                         </Col>
-
+                        <Col md={6}>
+                            { /* <RepositoryCardList repositories={store.gitHub.repositories} /> */}
+                        </Col>
                     </Row>
                 </Container>
             </div>
