b59310820b8e0a56415dfa85cb3a64c73404103c
gregorycernera <cernerag@gmail.com>
2020-02-02 15:14:54 -0500

    Created timeline for Resume page that is respective to personal info

--- cernera_me/src/components/pages/resume/ResumeForm.tsx
+++ cernera_me/src/components/pages/resume/ResumeForm.tsx
@@ -11,18 +11,46 @@ import 'react-vertical-timeline-component/style.min.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Work from '@material-ui/icons/Work';
+import School from '@material-ui/icons/School';
 
 const ResumeForm = ({ user, social, experience, education }: { user: string, social: SocialMediaUsernames, experience: any, education: any }) => {
 
     const jobData = experience.map(function(job: any, index: any) {
         return (
-            <ResumeItem name={job.name} title={job.title} subtitle={job.subtitle} logo={job.logo} />
+            
+
+            <VerticalTimelineElement
+                    contentStyle={{ background: '#e8e8e8', color: 'black', boxShadow: '7px 10px 12px grey' }}
+                    contentArrowStyle={{ borderRight: '10px solid #e8e8e8' }}
+                    iconStyle={{ background: '#42be65', color: '#fff' }}
+                    icon={<Work/>}
+                >
+                    <h3 className="vertical-timeline-element-title">{job.name}</h3>
+                    <h4 className="vertical-timeline-element-subtitle">{job.title}</h4>
+                    
+                    <p>
+                    {job.subtitle}
+                    </p>
+                </VerticalTimelineElement>
+
         );
     });
 
     const schoolData = education.map(function(school: any, index: any) {
         return (
-            <ResumeItem name={school.name} title={school.title} subtitle={school.subtitle} logo={school.logo} />
+            <VerticalTimelineElement
+                    contentStyle={{ background: '#e8e8e8', color: 'black', boxShadow: '7px 10px 12px grey'  }}
+                    contentArrowStyle={{ borderRight: '12px solid #e8e8e8' }}
+                    
+                    iconStyle={{ background: '#393939', color: '#fff' }}
+                    icon={<School/>}
+                >
+                    <h3 className="vertical-timeline-element-title">{school.name}</h3>
+                    <h4 className="vertical-timeline-element-subtitle">{school.title}</h4>
+                    <p>
+                    {school.subtitle}
+                    </p>
+                </VerticalTimelineElement>
         );
     });
 
@@ -31,116 +59,13 @@ const ResumeForm = ({ user, social, experience, education }: { user: string, soc
             <Sidebar sm={true} slideIn={false} user={user} social={social} />
             <div>
             
-            <VerticalTimeline>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-                    date="2011 - present"
-                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                    icon={<Work/>}
-                >
-                    <h3 className="vertical-timeline-element-title">Creative Director</h3>
-                    <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-                    <p>
-                    Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    date="2010 - 2011"
-                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                    
-                >
-                    <h3 className="vertical-timeline-element-title">Art Director</h3>
-                    <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-                    <p>
-                    Creative Direction, User Experience, Visual Design, SEO, Online Marketing
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    date="2008 - 2010"
-                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                    
-                >
-                    <h3 className="vertical-timeline-element-title">Web Designer</h3>
-                    <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
-                    <p>
-                    User Experience, Visual Design
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    date="2006 - 2008"
-                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                    
-                >
-                    <h3 className="vertical-timeline-element-title">Web Designer</h3>
-                    <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-                    <p>
-                    User Experience, Visual Design
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--education"
-                    date="April 2013"
-                    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                    
-                >
-                    <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
-                    <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-                    <p>
-                    Strategy, Social Media
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--education"
-                    date="November 2012"
-                    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                    
-                >
-                    <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
-                    <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-                    <p>
-                    Creative Direction, User Experience, Visual Design
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--education"
-                    date="2002 - 2006"
-                    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                    
-                >
-                    <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
-                    <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
-                    <p>
-                    Creative Direction, Visual Design
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-                    
-                />
-                </VerticalTimeline>
+            <VerticalTimeline className={styles_resume["vertical-timeline"]}>
 
-                <Container className={styles["landing-page__content__container"]}>
+                {jobData}
+                {schoolData}
 
-                    <Row className="h-100">
-                        <Col md={1} >
-                            <h1 className={styles_resume["resume-page__content__header"]}>Experience</h1>
-                        </Col>
-                    </Row>
-                    <Row>{jobData}</Row>
+            </VerticalTimeline>
 
-                    <Row className="h-100">
-                        <Col md={1} >
-                            <h1 className={styles_resume["resume-page__content__header"]}>Education</h1>
-                        </Col>
-                    </Row>
-                    <Row>{schoolData}</Row>
-
-                </Container>
             </div>
         </div>
     );
