bef5e16542bee00e02eb108487ad7526d12edbc8
gregorycernera <cernerag@gmail.com>
2020-03-27 22:53:30 -0400

    Added form on Contact page; Collects form data on submit

--- cernera_me/src/components/pages/contact/ContactPage.tsx
+++ cernera_me/src/components/pages/contact/ContactPage.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useHistory } from "react-router-dom";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
 import UserCard from "components/user/UserCard";
 import styles from './ContactPage.module.scss';
 import genStyle from "components/General.module.scss";
@@ -17,6 +17,25 @@ const ContactPage = ({ username, allUsers }: { username: string, allUsers: UserI
 
     const user: UserInfoType | undefined = findUser(allUsers, username);
 
+    let [email, setEmail]: any[] = useState({
+        name: '',
+        email_address: '',
+        subject: '',
+        message: ''
+    });
+
+    let handleChange = (e: any) => {
+        let id = e.target.id;
+        let value = e.target.value;
+        email[id] = value;
+        setEmail(email);
+    }
+
+    let handleClick = (e: any) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
     return (
         <>
             {user ?
@@ -26,17 +45,31 @@ const ContactPage = ({ username, allUsers }: { username: string, allUsers: UserI
                         <Row className={styles["contact-page__content__container__content"]}>
                             <Col md={12} className={[genStyle["horizontal-center"]].join(' ')}>
                                 <UserCard user={user} />
-                                <Card className={[styles["contact-page__content__container__content__card"], "shadow-sm"].join(' ')}>
-                                    <Card.Title className={styles["contact-page__content__container__content__card__title"]}>Contact Me</Card.Title>
-                                    <Card.Body className={styles["contact-page__content__container__content__card__body"]}>
-                                        {user.socialMedia.linkedin
-                                            ? <ContactItem contact_type={'LinkedIn'} contact={`${routes.ROUTE_LINKEDIN}/in/${user.socialMedia.linkedin}`} />
-                                            : null}
-                                        {user.showEmail && user.contact.work_email
-                                            ? <ContactItem contact_type={'Email'} contact={user.contact.work_email} />
-                                            : null}
-                                    </Card.Body>
-                                </Card>
+                                
+                                    <Form id="email-form">
+                                        <Form.Group controlId="exampleForm.ControlInput1">
+                                            <Form.Label>Your name</Form.Label>
+                                            <Form.Control id="name" type="text" placeholder="" onChange={handleChange}/>
+                                        </Form.Group>
+                                        <Form.Group controlId="exampleForm.ControlInput2">
+                                            <Form.Label>Your email</Form.Label>
+                                            <Form.Control id="email_address" type="email" placeholder="" onChange={handleChange}/>
+                                        </Form.Group>
+                                        <Form.Group controlId="exampleForm.ControlInput3">
+                                            <Form.Label>Subject</Form.Label>
+                                            <Form.Control id="subject" type="text" placeholder="" onChange={handleChange}/>
+                                        </Form.Group>
+                                        <Form.Group controlId="exampleForm.ControlTextarea1">
+                                            <Form.Label>Message</Form.Label>
+                                            <Form.Control id="message" as="textarea" rows="3" onChange={handleChange}/>
+                                        </Form.Group>
+
+                                        <Button variant="primary" type="submit" onClick={handleClick}>
+                                            Submit
+                                        </Button>
+
+                                    </Form>
+
                             </Col>
                         </Row>
                     </Container>
