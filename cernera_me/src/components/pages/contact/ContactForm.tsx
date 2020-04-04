4ff1a9286859451b11e7f47d49ed51bd66cb2289
Gregory-Cernera <gregory.cernera@ibm.com>
2020-04-04 01:16:41 -0400

    Added form validation and CSS changes

--- cernera_me/src/components/pages/contact/ContactForm.tsx
+++ cernera_me/src/components/pages/contact/ContactForm.tsx
@@ -8,6 +8,13 @@ import { toastr } from "react-redux-toastr";
 
 const ContactForm = ({userEmailAddress}: { userEmailAddress: any}) => {
 
+    let [errors, setErrors]: any[] = useState({
+        nameError: 'test',
+        emailError: '',
+        subjectError: '',
+        messageError: ''
+    })
+
     let [verify, setVerify]: any[] = useState({
         isVerified: false
     })
@@ -29,47 +36,109 @@ const ContactForm = ({userEmailAddress}: { userEmailAddress: any}) => {
 
     let handleClick = async (e: any) => {
         e.preventDefault();
-        let isVerified = verify['isVerified'];
+        
+        let isValid = validateFields();
+        if (!isValid) {return false}
 
-        if (isVerified === true) {
+        let isVerified = verify['isVerified'];
+        if (isVerified) {
             let sent = await sendEmail(email, userEmailAddress);
             
             if (sent) {
                 toastr.success("Email Sent", "Success");
-                setTimeout(() => {window.location.reload(false)},3000)
+                setTimeout(() => {window.location.reload(true)},3000)
             } else {
                 toastr.error("Email not sent", "Error")
             }
             
         } else {
-            toastr.warning("Please verify you are not a robot.", "");
-        }
+            const toastrType = 'warning';
+            const toastrOptions: any = {
+                icon: toastrType,
+                status: toastrType
+            }
+            toastr.light("Please verify you are not a robot.", "", toastrOptions);
+        }        
         
     }
 
     function verifyCallback(response: any) {
         if (response) {
-            verify['isVerified'] = true
+            verify['isVerified'] = true;
+            setVerify(verify);
         } else {
-            verify['isVerified'] = false
+            verify['isVerified'] = false;
+            setVerify(verify);
         }
     }
-    
 
+    function validateFields() {
+        let name = email['name'],
+            emailAddress = email['emailAddress'],
+            subject = email['subject'],
+            message = email['message'];
+
+        let nameError = '',
+            emailError = '',
+            subjectError = '',
+            messageError = '';
+
+        const toastrType = 'error';
+        const toastrOptions: any = {
+            icon: toastrType,
+            status: toastrType
+        }
+
+        // Validate name
+        if (!name) {
+            nameError = `A name is required`;
+            toastr.light(nameError, 'Error', toastrOptions);
+            return false
+        }
+
+        // Validate email
+        if (!emailAddress) {
+            emailError = `An email is required`;
+            toastr.light(emailError, 'Error', toastrOptions);
+            return false
+        } else if (!emailAddress.includes('@')) {
+            emailError = `Email address must contain an '@' symbol`;
+            toastr.light(emailError, 'Error', toastrOptions);
+            return false
+        }
+
+        // Validate subject
+        if (!subject) {
+            subjectError = `A subject is required`;
+            toastr.light(subjectError, 'Error', toastrOptions);
+            return false;
+        }
+
+        // Validate message
+        if (!message) {
+            messageError = `A message is required`;
+            toastr.light(messageError, 'Error', toastrOptions);
+            return false
+        }
+
+        return true
+
+    }
+    
     return (
         <>
             <Form className={styles["email-form"]}>
                 <Form.Row>
 
                     <Col>
-                        <Form.Group controlId="exampleForm.ControlInput1">
+                        <Form.Group>
                             <Form.Label>Your name</Form.Label>
                             <Form.Control className={styles["email-form__text-area"]} id="name" placeholder="" onChange={handleChange}/>
                         </Form.Group>
                     </Col>
 
                     <Col>
-                        <Form.Group controlId="exampleForm.ControlInput2">
+                        <Form.Group>
                             <Form.Label>Your email</Form.Label>
                             <Form.Control className={styles["email-form__text-area"]} id="emailAddress" placeholder="" onChange={handleChange}/>
                         </Form.Group>
@@ -77,12 +146,12 @@ const ContactForm = ({userEmailAddress}: { userEmailAddress: any}) => {
 
                 </Form.Row>
                 
-                <Form.Group controlId="exampleForm.ControlInput3">
+                <Form.Group>
                     <Form.Label>Subject</Form.Label>
                     <Form.Control className={styles["email-form__text-area"]} id="subject" placeholder="" onChange={handleChange}/>
                 </Form.Group>
 
-                <Form.Group controlId="exampleForm.ControlTextarea1">
+                <Form.Group>
                     <Form.Label>Message</Form.Label>
                     <Form.Control className={styles["email-form__text-area"]} id="message" as="textarea" rows="3" onChange={handleChange}/>
                 </Form.Group>
