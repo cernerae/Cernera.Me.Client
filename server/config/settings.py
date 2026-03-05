369f0d2865d905651b6c85e0e9cd052ed26a1076
cernerae <cernerae@gmail.com>
2026-03-05 00:40:54 -0500

    expose admin panel and collect static

--- server/config/settings.py
+++ server/config/settings.py
@@ -73,7 +73,7 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "/static/"
+STATIC_URL = "/api/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
