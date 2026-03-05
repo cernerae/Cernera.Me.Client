07103a252d4a96d942d6bd3de59f4a0b3120ba1c
cernerae <cernerae@gmail.com>
2026-03-04 23:55:16 -0500

    expose admin panel and collect static

--- server/config/settings.py
+++ server/config/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
