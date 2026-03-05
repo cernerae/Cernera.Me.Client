ccd047c2eab190d2b953c0119408637520dd8daa
cernerae <cernerae@gmail.com>
2026-03-05 01:57:42 -0500

    csrf issues

--- server/.env.tpl
+++ server/.env.tpl
@@ -2,6 +2,7 @@ SECRET_KEY=
 DEBUG=False
 ALLOWED_HOSTS=
 CORS_ALLOWED_ORIGINS=
+CSRF_TRUSTED_ORIGINS=
 DATABASE_URL=postgres://user:password@host:5432/dbname
 DB_SSLMODE=require
 DB_SSLROOTCERT=/path/to/ca-cert.pem
--- server/config/settings.py
+++ server/config/settings.py
@@ -79,6 +79,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=[])
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=[])
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
